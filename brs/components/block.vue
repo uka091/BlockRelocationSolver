0f330807e72a393a9ed818ba1c9c73a4490d14ad
francolic <fran.colic@gauss.dev>
2023-07-10 13:02:20 +0200

    chore: make block.vue

---
+++ brs/components/block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-16 h-16 flex items-center justify-center border border-4 border-blue-500 rounded bg-green-200">
+    <div class="text-2xl">
+      {{ number }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "block",
+  props: {
+    number: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- brs/pages/index.vue
+++ brs/pages/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <div></div>
+  <div class="flex flex-col items-center justify-center h-screen">
+
+    <div class="flex flex-col-reverse p-4 border-2 border-black w-fit bg-gray-200">
+
+      <div class="flex" v-for="column in columns" :key="column">
+        <div class="px-2" v-for="number in column" :key="number">
+          <Block  :number="number" ></Block>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'index',
+  data() {
+    return {
+      columns: [
+        [1, 2, 3, 4, 5, 6],    // Example column 1 with numbers
+        [4, 5, 6, 7, 1, 2],    // Example column 2 with numbers
+        [7, 8, 9],
+        [10,11,12], // Example column 3 with numbers
+      ]
+    };
+  },
+
 }
 </script>
